eb9c441d4d04f1afac71413eb80c7840c8cbfb36
AjaoPeter8 <oluwafemipeter02@gmail.com>
2022-10-18 15:33:27 -0700

    A program that prints _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,8 +3,15 @@
  * main - prints putchar
  * Return: 0
 */
-int _putchar(char c)
+int main(void)
 {
-putchar("_putchar\n");
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
 return (0);
 }
