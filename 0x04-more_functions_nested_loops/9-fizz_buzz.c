31f73514809908237dd0ab6ad24439dfcff55d94
AjaoPeter8 <oluwafemipeter02@gmail.com>
2022-10-21 21:12:14 -0700

    Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - filter number and words
+ * Return: 0
+*/
+
+int main(void)
+{
+int i;
+for (i = 0; i <= 100; i++)
+{
+if (i % 15 == 0)
+printf("FizzBuzz");
+else if (i % 5 == 0)
+printf("Buzz");
+else if (i % 3 == 0)
+printf("Fizz");
+else
+printf("%i", i);
+if ( i < 100)
+printf(" ");
+}
+printf("\n");
+return (0);
+}
