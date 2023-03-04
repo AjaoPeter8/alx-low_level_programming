2796dc7aadf0d63960e60a6285f4eb1a649e4211
AjaoPeter8 <oluwafemipeter02@gmail.com>
2023-03-04 15:19:26 -0800

    Proper indentation added

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,24 +8,24 @@
 
 void print_triangle(int size)
 {
-int i, space, hash;
-if (size <= 0)
-{
-_putchar('\n');
-}
-else
-{
-for (i = 1; i <= size; i++)
-{
-for (space = size - i; space >= 1; space--)
-{
-_putchar(' ');
-}
-for (hash = 1; hash <= i; hash++)
-{
-_putchar('#');
-}
-_putchar('\n');
-}
-}
+	int i, space, hash;
+		if (size <= 0)
+		{
+			_putchar('\n');
+		}
+		else
+		{
+			for (i = 1; i <= size; i++)
+			{
+				for (space = size - i; space >= 1; space--)
+				{
+					_putchar(' ');
+				}
+				for (hash = 1; hash <= i; hash++)
+				{
+					_putchar('#');
+				}
+				_putchar('\n');
+			}
+		}
 }
